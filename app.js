18bf0b80ef5e0bf40e3ed3536a7305bc0aa7b92d
WishAccquire <166239670+WishAccquire@users.noreply.github.com>
2025-07-12 20:27:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ connect();
 
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  next();
+});
  
 app.use(cors())
 app.use(morgan('dev'))
